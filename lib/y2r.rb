6f88d98584d4b5436c2f5890cdc370e3c5b4938a
David Majda <dmajda@suse.cz>
2013-06-11 09:50:58 +0200

    s/:include_file/:as_include_file/ when creating AST::YCP::Context
    This was probably a typo.

--- lib/y2r.rb
+++ lib/y2r.rb
@@ -8,8 +8,8 @@ require File.expand_path(File.dirname(__FILE__) + "/y2r/version")
 module Y2R
   def self.compile(input, options = {})
     ycp_context = AST::YCP::Context.new(
-      :export_private => options[:export_private],
-      :include_file   => options[:include_file]
+      :export_private  => options[:export_private],
+      :as_include_file => options[:as_include_file]
     )
     ruby_context = AST::Ruby::Context.new(:width => 80, :shift => 0)
 
