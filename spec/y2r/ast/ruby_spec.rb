5e95ea6d328715c874f49bc5cbefb5a67e35e586
David Majda <dmajda@suse.cz>
2013-07-02 16:41:50 +0200

    Ruby AST specs: Simplify Module specs

--- spec/y2r/ast/ruby_spec.rb
+++ spec/y2r/ast/ruby_spec.rb
@@ -492,36 +492,28 @@ module Y2R::AST::Ruby
     end
 
     describe "#to_ruby_no_comments" do
-      describe "basics" do
-        it "emits correct code" do
-          @node.to_ruby_no_comments(@context_default).should == [
-            "module M",
-            "  a = 42",
-            "  b = 43",
-            "  c = 44",
-            "end"
-          ].join("\n")
-        end
+      it "emits correct code" do
+        @node.to_ruby_no_comments(@context_default).should == [
+          "module M",
+          "  a = 42",
+          "  b = 43",
+          "  c = 44",
+          "end"
+        ].join("\n")
       end
 
-      describe "formatting" do
-        it "passes correct available space info to statements" do
-          node = Module.new(
-            :name       => "M",
-            :statements => check_context(
-              @statements,
-              :width => 78,
-              :shift => 0
-            ),
-          )
+      it "passes correct available space info to statements" do
+        node = Module.new(
+          :name       => "M",
+          :statements => check_context(@statements, :width => 78, :shift => 0),
+        )
 
-          node.to_ruby_no_comments(@context_default)
-        end
+        node.to_ruby_no_comments(@context_default)
       end
     end
 
     describe "#single_line_width_no_comments" do
-      it "returns infinity" do
+      it "returns correct value" do
         @node.single_line_width_no_comments.should == Float::INFINITY
       end
     end
